8dff55fe64c097b633369f4dc3a5756ca65f7a3a
Debbie Dávila <146867711+DebbieDavilaDev@users.noreply.github.com>
2024-02-16 18:41:44 +0000

    money exchange conversion table

---
+++ test.js
@@ -0,0 +1,13 @@
+
+
+test("One euro should be 1.07 dollars", function() {
+    const { fromEuroToDollar } = require('./app.js');
+    const dollars = fromEuroToDollar(3.5);
+ 
+    const expected = 3.5 * 1.07; 
+    expect(fromEuroToDollar(3.5)).toBe(3.745); 
+
+    ;})
+
+   
+
